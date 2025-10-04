3d45e9fc6b8bcff43457152dad36a7a94af8122a
Olha Goriachko <kitchak96@gmail.com>
2025-10-04 22:22:23 +0200

    all

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,22 @@
 class Storage {
+    #items;
     constructor(items) {
-        this.items = items;
+        this.#items = items;
 
     }
     getItems() {
-        return this.items;
+        return this.#items;
          
     }
     addItem (newItem){
         
-        this.items.push(newItem);
+        this.#items.push(newItem);
     }
     removeItem (itemToRemove){
-        const newItems = this.items.filter((item) => item !== itemToRemove);
-        this.items = newItems;
-     a
+    
+        const newItems = this.#items.filter((item) => item !== itemToRemove);
+        this.#items = newItems;
+     
     }
 
 }
